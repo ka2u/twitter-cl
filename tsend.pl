c21d8dea018821c65ad409944af4b71aacbd3fd1
Kazuhiro Shibuya <stevenlabs@gmail.com>
2010-04-05 12:38:46 +0900

    Added url shorten function by bit.ly.

--- tsend.pl
+++ tsend.pl
@@ -5,6 +5,8 @@ use warnings;
 use lib "./extlib/lib/perl5";
 use Net::Twitter::Lite;
 use Config::Pit;
+use LWP::UserAgent;
+use JSON;
 
 my $config = pit_get("twitter", require =>{
     "username" => "username",
@@ -17,6 +19,26 @@ my $nt = Net::Twitter::Lite->new(
 );
 
 my $word = $ARGV[0];
+my $shortened;
+if ($word =~ m{(http://.*)}) {
+    my $url = $1;
+    my $params = {
+        login => '',
+        apiKey => '',
+        uri => $url,
+        format => 'json',
+    };
+    my $endpoint = "http://api.bit.ly/v3/shorten";
+    my $ua = LWP::UserAgent->new;
+    my $res = $ua->post($endpoint, $params);
+    if ($res->is_success) {
+        $shortened = from_json($res->content);
+    }
+    else {
+        die "missed shorten.";
+    }
+}
+$word =~ s#http://.*#$shortened->{data}->{url}# unless $word =~ m{http://bit.ly/.*};
 print "can I tweet '$word'?\n";
 my $yn = <STDIN>;
 chomp $yn;
